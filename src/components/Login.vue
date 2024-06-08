e03fb5349364a97d37dc3f0e01eabcf5d7529560
Deo Duong <deo.duong@adurolife.com>
2024-06-08 17:15:15 +0700

    update api call

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,11 @@
 <template>
    <div>
       <h2>Đăng nhập</h2>
-      <form @submit="login">
-        <label for="username">Tài khoản:</label>
-        <input type="text" id="username" v-model="username">
-        <label for="password">Mật khẩu:</label>
-        <input type="password" id="password" v-model="password">
-        <button type="submit">Đăng nhập</button>
-      </form>
+      <label for="username">Tài khoản:</label>
+      <input type="text" id="username" v-model="username">
+      <label for="password">Mật khẩu:</label>
+      <input type="password" id="password" v-model="password">
+      <button @click="login">Đăng nhập</button>
       <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
     </div>
   </template>
@@ -25,20 +23,8 @@
      methods: {
        async login() {
          try {
-           console.log ('hello');
-            axios.post('http://localhost:8000/api/login', {
-             email: this.username,
-             password: this.password
-           })
-           .then (response => {
-            console.log(response.data);
-           }).catch(error =>{
-            console.log('Đăng nhập thất bại',error);
-           }
-
-           )
-           // Xử lý kết quả khi đăng nhập thành công
-        //    console.log(response.data);
+          const response = await axios.post('http://localhost:9001/api/login');
+          console.log(response.data);
          } catch (error) {
            this.errorMessage = 'Đăng nhập không thành công. Vui lòng kiểm tra thông tin đăng nhập của bạn.';
          }
